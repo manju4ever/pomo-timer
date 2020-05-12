3b0c85f481116d296296ac18c654bb694495c0d2
Manjunath D <manju.desappa@gmail.com>
2020-05-12 14:01:30 +0530

    Pomodoro Timer - v1

---
+++ README.md
@@ -0,0 +1,14 @@
+> A simple timer to increase focus and productivity (The Pomodoro Technique)
+
+![snapshot](https://user-images.githubusercontent.com/9355984/81475283-cd5d1c80-9228-11ea-85ac-343ec1063c12.png)
+
+## How do you run the app ?
+
+* ### Install Dependencies
+   `$npm install`
+
+* ### Watch for live changes
+   `$npm run start`
+
+* ### Build
+    `$npm run build`
---
+++ src/Stopwatch.js
@@ -0,0 +1,86 @@
+const stopwatch = (timeToEnd = 10, unit = 's') => {
+
+  if(!timeToEnd || !(Boolean(Number(timeToEnd)))) {
+    throw new Error(`Expected time to be of type Number instead got ${typeof timeToEnd}`)
+  }
+
+  const parsedTimeToEnd = parseInt(Number(timeToEnd));
+
+  let cancelAfter = 0, 
+      divisor = 1000,
+      elapsed_total_seconds = 0,
+      elapsed_seconds = 0,
+      elapsed_min = 0,
+      counter = null,
+      isRunning = false,
+      isTimerEnded = false;
+
+    switch(unit) {
+      case 's' : cancelAfter = parsedTimeToEnd * 1000; break;
+      case 'm' : {
+          cancelAfter = parsedTimeToEnd * 60 * 1000; 
+          divisor = divisor * 60; 
+          break;
+      }
+      default: cancelAfter = parsedTimeToEnd * 1000
+    }
+
+    function _start() {
+        
+        if(isTimerEnded) _reset();
+        if(isRunning) return;
+
+        isRunning  = true;
+        isTimerEnded = false;
+
+        console.log(`Timer Started !`);
+        console.log(`Timer will stop in `, cancelAfter / divisor, unit);
+        
+        counter = setInterval(() => {
+          elapsed_total_seconds++;
+          elapsed_seconds++;
+          elapsed_min = Math.floor(elapsed_total_seconds / 60);
+          
+          // Reset the elapsed_seconds again
+          if(elapsed_seconds === 60) {
+            elapsed_seconds = 0;
+          }
+
+          // check elapsed_total_seconds equals cancelAfter
+          if(elapsed_total_seconds * 1000 === cancelAfter) {
+            _stop();
+            isRunning = false;
+          }
+        }, 1000);
+    }
+
+    function _stop() {
+      clearInterval(counter);
+      console.log(`Timer stopped !`);
+    }
+
+    function _reset() {
+      _stop();
+      elapsed_seconds = 0;
+      elapsed_total_seconds = 0;
+      elapsed_min = 0;
+      isTimerEnded = true;
+      isRunning = false;
+    }
+
+    function _timeRemaining() { 
+      return `${Math.floor((cancelAfter - elapsed_total_seconds)/(60 * 1000))}m ${60 - (elapsed_seconds || 60)}s`;
+    }
+
+    return {
+      start: _start,
+      stop: _stop,
+      reset: _reset,
+      peek: () => `${elapsed_min}m ${elapsed_seconds}s`,
+      isStopped: () => isTimerEnded,
+      isRunning: () => isRunning,
+      timeRemaining: _timeRemaining,
+    };
+};
+
+export default stopwatch;
