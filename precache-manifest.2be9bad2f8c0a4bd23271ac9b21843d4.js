2a9f9b28c89de94029dec1a1fb02d8a913dd9fcf
Manjunath D <manju.desappa@gmail.com>
2020-05-12 14:02:20 +0530

    Updates

---
+++ precache-manifest.2be9bad2f8c0a4bd23271ac9b21843d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891daa5dde81b8818cb6ae9ed67ec21f",
+    "url": "/pomo-timer/index.html"
+  },
+  {
+    "revision": "8f31b7356d62416c2147",
+    "url": "/pomo-timer/static/css/main.f4e4ad78.chunk.css"
+  },
+  {
+    "revision": "57dc5066a7d8c925ef7d",
+    "url": "/pomo-timer/static/js/2.e1bba15b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomo-timer/static/js/2.e1bba15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f31b7356d62416c2147",
+    "url": "/pomo-timer/static/js/main.347ef285.chunk.js"
+  },
+  {
+    "revision": "521469176eabf4bf63f7",
+    "url": "/pomo-timer/static/js/runtime-main.54c947ed.js"
+  }
+]);
